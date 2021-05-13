600a11dfb5007fd2dc7d22628bb47ff0ee8ac073
Maxim <mbzozn@mail.ru>
2021-05-13 16:10:36 +0300

    Change Name

--- SMWF/SMWF/Form2.Designer.cs
+++ SMWF/SMWF/Form2.Designer.cs
@@ -120,7 +120,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Свой выбор";
             this.ResumeLayout(false);
             this.PerformLayout();
 
